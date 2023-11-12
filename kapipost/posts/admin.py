6e83d4741f92d5aa164ddfadd7c37291b3783b3f
Sergei Borovskii <serezhka95@gmail.com>
2023-11-12 21:16:48 +0600

    Admin configuration of posts-app

--- kapipost/posts/admin.py
+++ kapipost/posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    # We list the fields that should be displayed in the admin panel
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    # Interface to search through post-text
+    search_fields = ('text',)
+    # Filtration options
+    list_filter = ('pub_date', 'author')
+    # Edit options
+    list_editable = ('group',)
+    empty_value_display = '-empty-'
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
