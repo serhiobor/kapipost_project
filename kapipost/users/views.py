3230f5cbeece7d53bfc895c895cfb19189736e64
Sergei Borovskii <serezhka95@gmail.com>
2023-11-12 21:36:22 +0600

    Users-app configs
    Added forms and views

--- kapipost/users/views.py
+++ kapipost/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CreationForm
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    '''Login view'''
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Log In'
+        return context
+
+
+class SignUp(CreateView):
+    '''SignUp view'''
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Sign Up'
+        return context
