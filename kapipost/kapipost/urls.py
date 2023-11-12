7c62557db12cc844f71c840350aecc8b82c1236e
Sergei Borovskii <serezhka95@gmail.com>
2023-11-12 21:30:16 +0600

    Urls files added
    URLs conf-s at:
    - about
    - kapipost
    - posts
    - users
    
    PS. Dev-server don't work

--- kapipost/kapipost/urls.py
+++ kapipost/kapipost/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+handler403 = 'core.views.csrf_failure'
+handler404 = 'core.views.page_not_found'
 
 urlpatterns = [
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls', namespace='users')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('about/', include('about.urls', namespace='about')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
